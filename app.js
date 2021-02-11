8ee14b2462f25d8fb0f3bb5c3f48737ae23a9452
NihadZeidan <nihadzeidan@gmail.com>
2021-02-11 14:53:57 +0200

    Solve problem 4

--- app.js
+++ app.js
@@ -82,15 +82,20 @@ IMPORTANT DETAIL: You may not use the arithmetic operator + in this function. To
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSumArray() function and see if the test passes.*/
 
 // Write your code here
-let testArray = [2, 3, 4]; //eslint-disable-line
+//eslint-disable-line
+let testArray = [2, 3, 4]; 
 
 function sumArray(sumArr) {
+  let sum1= testArray
+  let summing = sum(2, 3)[0]
+  let finalsumming = sum(summing, 4)[0]
+  return[finalsumming, `2,3,4 was passed in as an array of numbers, and ${finalsumming} is their sum.`]
   //eslint-disable-line
 }
 
 // Here is the test for sumArray(); uncomment it to run it
 
-// testSumArray(testArray);
+testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
